66273c7ef4d3d7f073ec346ab18b937dd8bd3295
kikisal <kikisal91@gmail.com>
2025-05-13 00:21:42 +0200

    Started discography page

--- discography.php
+++ discography.php
@@ -5,15 +5,26 @@
 <?php $noFancyStuff = true; ?>
 <?php require_once __DIR__ . "/components/head.php"; ?>
 
-<body opacity-animation class="state-disappear" use-img-animation>
-    
+<body opacity-animation class="state-disappear" use-img-animation>    
     <?php require_once __DIR__ . "/components/mobile-header.php"; ?>
     <?php require_once __DIR__ . "/components/header.php"; ?>
 
-    <div class="content-wrapper content-shadow">        
-        <div class="main-content">
-            <h1 style="margin-top:80px">Discografia!</h1>
+    <div class="content-wrapper h-100 pd-81">
+        <div class="content-section site-directory">
+            <div class="fixed-content content-padded">
+                <div class="section-headline-content">
+                    <p class="site-dir"><a href="<?= SITE_URL; ?>/">DJ Sanny J</a> / Discografia</p>
+                    <h1 class="section-headline">Discografia</h1>
+                </div>
+            </div>
         </div>
+
+        <div class="content-section product-browsing">
+            <div class="fixed-content content-padded">
+                <h1 class="section-headline">BROWSE</h1>
+            </div>
+        </div>
+        
         <div class="flex-grow"></div>
         <?php require_once __DIR__ . "/components/footer.php"; ?>
     </div>
